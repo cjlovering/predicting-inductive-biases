110292703ccaf20a4c168124c8e3cd2fc297590e
Edoardo Pona <edoardo.pona@gmail.com>
2023-07-08 16:06:26 +0100

    dataset generating script

---
+++ run_datasets.sh
@@ -0,0 +1,25 @@
+python sva.py --template base --weak lexical
+python sva.py --template base --weak agreement
+python sva.py --template base --weak plural
+python sva.py --template hard --weak lexical
+python sva.py --template hard --weak agreement
+python sva.py --template hard --weak plural
+python sva.py --template hard --weak length
+python npi.py --weak lexical
+python npi.py --weak plural
+python npi.py --weak tense
+python npi.py --weak length
+python toy.py --true_property 1
+python toy.py --true_property 2
+python toy.py --true_property 3
+python toy.py --true_property 4
+python toy.py --true_property 5
+python gap.py --template base --weak length
+python gap.py --template base --weak lexical
+python gap.py --template base --weak plural
+python gap.py --template base --weak tense
+python gap.py --template hard --weak none
+python gap.py --template hard --weak length
+python gap.py --template hard --weak lexical
+python gap.py --template hard --weak plural
+python gap.py --template hard --weak tense
